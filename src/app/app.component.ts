c2aa2dd3c59df944e9eb86ebfaa840944fca7a5d
nanpipat.k@gmail.com <nanpipat.k@gmail.com>
2020-12-17 14:38:07 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JwksValidationHandler, OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sso-app';
+  constructor(private oauth: OAuthService) {
+    this.configSigleSignIn();
+  }
+  configSigleSignIn() {
+    this.oauth.configure(authCodeFlowConfig);
+    this.oauth.tokenValidationHandler = new JwksValidationHandler();
+    this.oauth.loadDiscoveryDocumentAndTryLogin();
+  }
+  login() {
+    this.oauth.initImplicitFlow();
+  }
+  logout() {
+    this.oauth.logOut();
+  }
+
+  get token(){
+    let claim: any = this.oauth.getIdentityClaims();
+    return claim ? claim : null;
+  }
 }
